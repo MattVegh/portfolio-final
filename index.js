47186259dc9d31062b4fa4cfc55e75e6ea973e32
Matt Vegh <mdv240@gmail.com>
2020-01-28 21:25:28 -0600

    pop up

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 function imagePop() {
     $(".trigger_popup").click(function(){
-       $('.popup-box').show();
+        let imgSrc = $(this).find(".app-ss").attr("src")
+        console.log(imgSrc)
+        $(".popup-img-link").attr("href", imgSrc)
+        $(".popup-ss").attr("src", imgSrc)
+       $(".popup-box").show();
     });
-    $('.popup-box').click(function(){
-        $('.popup-box').hide();
+    $(".popup-box").click(function(){
+        $(".popup-box").hide();
     });
-    $('.popup-close-button').click(function(){
-        $('.popup-box').hide();
+    $(".popup-close-button").click(function(){
+        $(".popup-box").hide();
     });
 };
 
